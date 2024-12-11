e133370a59ed97c9861bd3b61fad35b22b3849d5
D3V1L1810 <107905817+D3V1L1805@users.noreply.github.com>
2024-12-11 15:22:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ImageUpload from './ImageUpload';
+
+const App = () => {
+    return (
+        <div>
+            <ImageUpload />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/ImageUpload.js
@@ -0,0 +1,67 @@
+// src/ImageUpload.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import './ImageUpload.css'; // Import CSS for styling
+
+const ImageUpload = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [responseText, setResponseText] = useState('');
+
+    const handleImageChange = (event) => {
+        setSelectedImage(event.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        if (!selectedImage) return;
+
+        const formData = new FormData();
+        formData.append('file', selectedImage);
+
+        try {
+            const response = await axios.post('https://ies-backend-rxth.onrender.com/upload-photo', formData);
+            setResponseText(response.data.description);
+        } catch (error) {
+            console.error('Error uploading image:', error);
+            setResponseText('Error uploading image');
+        }
+    };
+
+    return (
+        
+        <div className="upload-container">
+            <div className='Header'>
+                Welcome to Image Interpreter AI
+            </div>
+            <div className='SubHeader'>
+                Upload Image and get image description
+            </div>
+            <div className='SH'>
+            <div className='SH1'>
+                Safe
+            </div>
+            <strong>&#183;</strong>
+            <div className='SH1'>
+                Secure
+            </div>
+            <strong>&#183;</strong>
+            <div className='SH1'>
+                Reliable
+            </div>
+            </div>
+            <input type="file" onChange={handleImageChange} />
+            <button onClick={handleUpload}>Upload</button>
+            {selectedImage && (
+                <div className="image-preview">
+                    <img src={URL.createObjectURL(selectedImage)} alt="Preview" />
+                </div>
+            )}
+            {responseText && (
+                <div className={`description-box ${responseText ? 'visible' : ''}`}>
+                    {responseText}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ImageUpload;
